6a1f00e77745b591eece81b4cbd0a29503bbe4f8
kevinanlipscomb <kevinandrewlipscomb@vt.edu>
2002-05-02 02:13:35 +0000

    Added ability to log old donations.&nbsp; This also makes several new kinds of queries possible.

--- qryCreDonation.sql
+++ qryCreDonation.sql
@@ -4,14 +4,14 @@
 # http://phpmyadmin.sourceforge.net/ (download page)
 #
 # Host: localhost
-# Generation Time: Apr 20, 2002 at 06:30 PM
+# Generation Time: May 01, 2002 at 10:02 PM
 # Server version: 3.23.46
 # PHP Version: 4.1.2
 # Database : `keyclick`
 # --------------------------------------------------------
 
 #
-# Table structure for table `donation` and `x_donation`
+# Table structure for tables `donation` and `x_donation`
 #
 
 CREATE TABLE donation (
@@ -24,6 +24,7 @@ CREATE TABLE donation (
   entered_by varchar(64) NOT NULL default '',
   timestamp timestamp(14) NOT NULL,
   per_clerk_seq_num bigint(20) unsigned NOT NULL default '0',
+  date date NOT NULL default '0000-00-00',
   PRIMARY KEY  (entered_by,per_clerk_seq_num),
   KEY id (id)
 ) TYPE=MyISAM;
@@ -38,6 +39,8 @@ CREATE TABLE x_donation (
   entered_by varchar(64) NOT NULL default '',
   timestamp timestamp(14) NOT NULL,
   per_clerk_seq_num bigint(20) unsigned NOT NULL default '0',
+  date date NOT NULL default '0000-00-00',
   PRIMARY KEY  (entered_by,per_clerk_seq_num),
   KEY id (id)
 ) TYPE=MyISAM;
+
