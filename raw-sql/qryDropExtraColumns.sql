f91039639417ed466df6a4bad01bd1c93cc0a72d
kevinanlipscomb <kevinandrewlipscomb@vt.edu>
2002-07-19 19:23:19 +0000

    Shaved several unneeded columns from the ROD and revised RODBY* analyses not to refer to the shaved-off columns.

---
+++ raw-sql/qryDropExtraColumns.sql
@@ -0,0 +1,11 @@
+ALTER TABLE `resident`
+  DROP `unk1`,
+  DROP `subdrive`,
+  DROP `unk2`,
+  DROP `salutation`,
+  DROP `unk3`,
+  DROP `unk4`,
+  DROP `unk5`,
+  DROP `unk6`,
+  DROP `unk7`,
+  DROP `unk8`;
