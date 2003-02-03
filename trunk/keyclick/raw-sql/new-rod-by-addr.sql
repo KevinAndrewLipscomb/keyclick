da4d840eda3ee655aa228babc88157d708328302
kevinanlipscomb <kevinanlipscomb@fcafd3cc-6610-0410-92fd-86a883f3e63b>
2003-02-03 19:04:47 +0000

    Alpha changes.

--- trunk/keyclick/raw-sql/new-rod-by-addr.sql
+++ trunk/keyclick/raw-sql/new-rod-by-addr.sql
@@ -38,22 +38,65 @@ select distinct
 from x_city_has_streets
    left join x_street_section using (street_id);
 #
-drop table if exists x_rod_by_addr;
-create table x_rod_by_addr
+drop table if exists x_query_control;
+create table x_query_control
+   (
+   id mediumint(8) unsigned NOT NULL auto_increment,
+   state_name char(255) default '',
+   city_name char(255) default '',
+   street_name char(127) default '',
+   street_section_id mediumint(8) unsigned default '0',
+   PRIMARY KEY  (id)
+   ) TYPE=MyISAM;
+insert into x_query_control
 select
-   x_state_has_cities.state_name as state,
-   x_state_has_cities.city_name as city,
-   x_city_has_streets.street_name as street,
-   x_resident.house_num as house_num,
-   x_resident.name as name
+   null as id,
+   x_state_has_cities.state_name as state_name,
+   x_state_has_cities.city_name as city_name,
+   x_city_has_streets.street_name as street_name,
+   x_street_has_sections.section_id as street_section_id
 from x_state_has_cities
    left join x_city_has_streets using (city_id)
    left join x_street_has_sections using (street_id)
-   left join x_resident on (x_resident.street_section_id=x_street_has_sections.section_id)
    left join x_city on (x_city.id=x_city_has_streets.city_id)
    left join x_street on (x_street.id=x_street_has_sections.street_id)
+;
+drop table if exists x_rod_by_addr;
+create table x_rod_by_addr
+select
+   x_resident.id as id,
+   x_resident.name as name,
+   x_resident.house_num as house_num,
+   x_query_control.street_name as street,
+   x_query_control.city_name as city,
+   x_query_control.state_name as state,
+   concat(x_zip5.code,'-',x_street_section.plus4part) as zip
+from x_query_control
+   left join x_resident on (x_resident.street_section_id=x_query_control.street_section_id)
+   left join x_zip5 on (x_zip5.id=x_resident.zip5_id)
+   left join x_street_section on (x_street_section.id=x_resident.street_section_id)
 where
-   x_city_has_streets.street_name rlike '^[A-M].*'
+   x_query_control.id <= 2662
+order by
+   state,
+   city,
+   street,
+   house_num;
+insert into x_rod_by_addr
+select
+   x_resident.id as id,
+   x_resident.name as name,
+   x_resident.house_num as house_num,
+   x_query_control.street_name as street,
+   x_query_control.city_name as city,
+   x_query_control.state_name as state,
+   concat(x_zip5.code,'-',x_street_section.plus4part) as zip
+from x_query_control
+   left join x_resident on (x_resident.street_section_id=x_query_control.street_section_id)
+   left join x_zip5 on (x_zip5.id=x_resident.zip5_id)
+   left join x_street_section on (x_street_section.id=x_resident.street_section_id)
+where
+   x_query_control.id > 2662
 order by
    state,
    city,
