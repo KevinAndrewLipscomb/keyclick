f455a8a55cc3363f3bf0aa2f235c66eab1498625
Kevin Andrew Lipscomb <kevinandrewlipscomb@vt.edu>
2024-06-20 17:34:24 -0400

    Revised keyclick--import-vb-ems-rxx-DISTRICT-targets.sql to perform additional address scrubbing.  Improved 0-REFRESH.ps1 and work.ps1 based on lessons learned from other projects.  Simplified MSBUILDPROJ file.

--- work.ps1
+++ work.ps1
@@ -1,6 +1,6 @@
 Set-Location "C:\Inetpub\wwwroot\keyclick"
 Start-Process -WindowStyle Maximized "."
-Start-Process -WindowStyle Maximized keyclick.sln
+Start-Process -WindowStyle Maximized .\keyclick.sln
 IF (Test-Path "C:\Program Files\MySQL\MySQL Workbench\MySQLWorkbench.exe")
   {
   Start-Process -WindowStyle Maximized "C:\Program Files\MySQL\MySQL Workbench\MySQLWorkbench.exe"
